34a5924b70c94f04d59e93b943d6195a9adc4a1f
Jake 'Sid' Smith <jake@dabapps.com>
2017-04-28 15:14:59 +0100

    Line length in demo

--- docs/src/ts/text.tsx
+++ docs/src/ts/text.tsx
@@ -36,23 +36,32 @@ export const Text = () => (
           Header 1
         </h6>
         <p>
-          Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipiscing elit. Suspendisse luctus scelerisque arcu, lacinia porttitor justo pretium nec. Aliquam <a href="#">erat volutpat</a>. Integer feugiat erat ut felis imperdiet, at aliquam quam auctor. Donec dictum pulvinar porta. Integer dui nibh, commodo vitae sodales eget, varius ac nulla. Morbi tempus risus ut hendrerit mattis. Nunc eu libero elementum ipsum accumsan interdum at eu ligula. Nunc accumsan tortor libero, sed dapibus augue hendrerit et. Donec vel nunc quis magna euismod facilisis. Ut mi est, aliquet non ultrices eget, euismod non risus. <a href="#">Vivamus vel nisi et enim efficitur interdum</a>.
+          Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipiscing elit. Suspendisse luctus scelerisque arcu,
+          lacinia porttitor justo pretium nec. Aliquam <a href="#">erat volutpat</a>. Integer feugiat erat ut felis
+          imperdiet, at aliquam quam auctor. Donec dictum pulvinar porta. Integer dui nibh, commodo vitae sodales eget,
+          varius ac nulla. Morbi tempus risus ut hendrerit mattis. Nunc eu libero elementum ipsum accumsan interdum at
+          eu ligula. Nunc accumsan tortor libero, sed dapibus augue hendrerit et. Donec vel nunc quis magna euismod
+          facilisis. Ut mi est, aliquet non ultrices eget, euismod non risus. <a href="#">Vivamus vel nisi et enim
+          efficitur interdum</a>.
         </p>
         <p>
           <i>
-            Sed lobortis id nisl nec laoreet. Nulla mattis fermentum augue vitae tempus. Cras lorem leo, finibus et augue nec, sagittis vulputate turpis. Morbi tincidunt risus in elit dignissim, at luctus ligula feugiat.
+            Sed lobortis id nisl nec laoreet. Nulla mattis fermentum augue vitae tempus. Cras lorem leo, finibus et
+            augue nec, sagittis vulputate turpis. Morbi tincidunt risus in elit dignissim, at luctus ligula feugiat.
           </i>
         </p>
         <p>
           <strong>
             <i>
-              Quisque auctor rhoncus rutrum. Maecenas quis dui ut lectus tempor tincidunt. In nunc ex, condimentum in risus nec, porta maximus arcu. Nam dictum ipsum in dui efficitur condimentum.
+              Quisque auctor rhoncus rutrum. Maecenas quis dui ut lectus tempor tincidunt. In nunc ex, condimentum in
+              risus nec, porta maximus arcu. Nam dictum ipsum in dui efficitur condimentum.
             </i>
           </strong>
         </p>
         <p>
           <strong>
-            In non pellentesque arcu. Maecenas lacinia semper lectus vitae sagittis. Mauris metus nibh, posuere at enim porttitor, maximus vehicula quam.
+            In non pellentesque arcu. Maecenas lacinia semper lectus vitae sagittis. Mauris metus nibh, posuere at enim
+            porttitor, maximus vehicula quam.
           </strong>
         </p>
       </Column>
