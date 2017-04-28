93c88a3e149d7c32a7701f811108ec9b46741782
Jake 'Sid' Smith <jake@dabapps.com>
2017-04-28 11:55:23 +0100

    Improve code formatting

--- src/ts/code-block.tsx
+++ src/ts/code-block.tsx
@@ -2,7 +2,7 @@ import * as classNames from 'classnames';
 import * as hljs from 'highlight.js';
 import * as React from 'react';
 
-const MATCHES_INITIAL_INDENTATION = /^([^\n\S]*)\S/gm;
+const MATCHES_INITIAL_INDENTATION = /^([^\S\n]*)\S/;
 const MATCHES_BLANK_FIRST_LINE = /^\s*\n/;
 const MATCHES_BLANK_LAST_LINE = /\n\s*$/;
 
@@ -32,19 +32,25 @@ export class CodeBlock extends React.Component<IProps, any> {
     }
   }
 
+  public formatCode (code: string) {
+    const codeWithoutLeadingOrTrailingEmptyLines = code
+      .replace(MATCHES_BLANK_FIRST_LINE, '')
+      .replace(MATCHES_BLANK_LAST_LINE, '');
+
+    const initialIndentation: RegExpExecArray | null =
+      MATCHES_INITIAL_INDENTATION.exec(codeWithoutLeadingOrTrailingEmptyLines);
+
+    return initialIndentation ?
+      codeWithoutLeadingOrTrailingEmptyLines.replace(new RegExp(`^${initialIndentation[1]}`, 'gm'), '') :
+      codeWithoutLeadingOrTrailingEmptyLines;
+  }
+
   public render () {
     const { children, className, language, ...remainingProps } = this.props;
     const languageClassName = language && `language-${language}`;
 
-    const initialIndentation: RegExpExecArray | null =
-      typeof children === 'string' ? MATCHES_INITIAL_INDENTATION.exec(children) : null;
-
-    const content = typeof children === 'string' && initialIndentation ?
-      children
-        .replace(MATCHES_BLANK_FIRST_LINE, '')
-        .replace(MATCHES_BLANK_LAST_LINE, '')
-        .replace(new RegExp(initialIndentation[1], 'g'), '') :
-      children;
+    const childrenIsString = typeof children === 'string';
+    const content = typeof children === 'string' ? this.formatCode(children) : children;
 
     return (
       <pre
