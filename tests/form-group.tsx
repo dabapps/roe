817ad91d6f3a6e1fbe8a783e2d3123be2eb0674f
Jake 'Sid' Smith <jake@dabapps.com>
2017-05-02 09:22:43 +0100

    Form group tests

---
+++ tests/form-group.tsx
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import { FormGroup } from '../src/ts/';
+
+describe('FormGroup', () => {
+
+  it('should match snapshot', () => {
+    const tree = renderer.create(
+      <FormGroup />
+    );
+
+    expect(tree).to.matchSnapshot();
+  });
+
+  it('should use a block prop (boolean) as a class name', () => {
+    const tree = renderer.create(
+      <FormGroup block />
+    );
+
+    expect(tree).to.matchSnapshot();
+  });
+
+});
