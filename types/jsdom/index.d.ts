5ca09a78e8079dab7ba20e88b0a009f08478d73c
Jake 'Sid' Smith <jake@dabapps.com>
2017-05-02 13:59:38 +0100

    Remove declare from ambient context

--- types/jsdom/index.d.ts
+++ types/jsdom/index.d.ts
@@ -1,6 +1,6 @@
 declare module 'jsdom' {
 
-  export declare class JSDOM extends Document {
+  export class JSDOM extends Document {
     public window: Window;
     constructor (markup?: string, config?: any);
   }
