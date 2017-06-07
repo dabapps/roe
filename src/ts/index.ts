312566e402049ad6a17e14f694f325cccf9c53c5
Jake <jake@dabapps.com>
2017-06-07 11:12:17 +0100

    Export all component by name

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,15 +4,34 @@ export { Button } from './button';
 export { CodeBlock } from './code-block';
 export { Column } from './column';
 export { Container } from './container';
-export * from './content-box';
+export {
+  ContentBox,
+  ContentBoxHeader,
+} from './content-box';
 export { DabIpsum } from './dab-ipsum';
 export { FormGroup } from './form-group';
 export { InputGroup } from './input-group';
 export { InputGroupAddon } from './input-group-addon';
-export * from './modals';
+export {
+  Modal,
+  ModalCloseIcon,
+  ModalFooter,
+  ModalHeader,
+  ModalRenderer,
+} from './modals';
 export { Row } from './row';
 export { Section } from './section';
 export { SpacedGroup } from './spaced-group';
-export * from './tabs';
-export * from './tables';
+export {
+  Tab
+  Tabs,
+} from './tabs';
+export {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from './tables';
 export { Well } from './well';
