b51fab5394f3ede2b4071d690dc00e1bfc6e32a2
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-11 21:47:50 +0100

    Test sections config

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,5 +2,15 @@ module.exports = {
   components: 'src/ts/**/*.{ts,tsx}',
   ignore: ['**/ts/index.ts', '**/utils/**'],
   propsParser: require('react-docgen-typescript').parse,
-  webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js')
+  webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js'),
+  sections: [
+    {
+      name: 'Grid',
+      components: './src/ts/components/grid/**/*.tsx'
+    },
+    {
+      name: 'Misc',
+      components: './src/ts/**/*.tsx'
+    }
+  ]
 };
