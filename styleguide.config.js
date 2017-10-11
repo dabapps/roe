e69cb61f87f0f96f9843c6cccd29c81aa461cdde
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-11 23:24:38 +0100

    Order sections alphabetically

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,25 +1,49 @@
+const sections = [
+  {
+    name: 'Content',
+    components: 'src/ts/components/content/**/*.tsx'
+  },
+  {
+    name: 'Grid',
+    components: 'src/ts/components/grid/**/*.tsx'
+  },
+  {
+    name: 'Tabs',
+    components: 'src/ts/components/tabs/**/*.tsx'
+  },
+  {
+    name: 'Tables',
+    components: 'src/ts/components/tables/**/*.tsx'
+  },
+  {
+    name: 'Forms',
+    components: 'src/ts/components/forms/**/*.tsx'
+  },
+  {
+    name: 'Prototyping',
+    components: 'src/ts/components/prototyping/**/*.tsx'
+  },
+  {
+    name: 'Misc',
+    components: 'src/ts/components/*.tsx'
+  }
+];
+
 module.exports = {
   title: 'Roe',
   components: 'src/ts/components/**/*.{ts,tsx}',
   ignore: [],
   propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
   webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js'),
-  sections: [
-    {
-      name: 'Grid',
-      components: 'src/ts/components/grid/**/*.tsx'
-    },
-    {
-      name: 'Tabs',
-      components: 'src/ts/components/tabs/**/*.tsx'
-    },
-    {
-      name: 'Forms',
-      components: 'src/ts/components/forms/**/*.tsx'
-    },
-    {
-      name: 'Misc',
-      components: 'src/ts/components/*.tsx'
+  sections: sections.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
     }
-  ]
+
+    if (a.name < b.name) {
+      return -1;
+    }
+
+    return 0;
+  })
 };
