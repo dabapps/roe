9b25954da6f65fb3da4f4e54a5519018800fad30
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-11 21:36:41 +0100

    Adjust formatCode indentation

--- src/ts/code-block.tsx
+++ src/ts/code-block.tsx
@@ -19,17 +19,17 @@ export interface CodeBlockProps extends HTMLProps<HTMLDivElement> {
 }
 
 export function formatCode (code: string) {
-    const codeWithoutLeadingOrTrailingEmptyLines = code
-      .replace(MATCHES_BLANK_FIRST_LINE, '')
-      .replace(MATCHES_BLANK_LAST_LINE, '');
+  const codeWithoutLeadingOrTrailingEmptyLines = code
+    .replace(MATCHES_BLANK_FIRST_LINE, '')
+    .replace(MATCHES_BLANK_LAST_LINE, '');
 
-    const initialIndentation: RegExpExecArray | null =
-      MATCHES_INITIAL_INDENTATION.exec(codeWithoutLeadingOrTrailingEmptyLines);
+  const initialIndentation: RegExpExecArray | null =
+    MATCHES_INITIAL_INDENTATION.exec(codeWithoutLeadingOrTrailingEmptyLines);
 
-    return initialIndentation ?
-      codeWithoutLeadingOrTrailingEmptyLines.replace(new RegExp(`^${initialIndentation[1]}`, 'gm'), '') :
-      codeWithoutLeadingOrTrailingEmptyLines;
-  }
+  return initialIndentation ?
+    codeWithoutLeadingOrTrailingEmptyLines.replace(new RegExp(`^${initialIndentation[1]}`, 'gm'), '') :
+    codeWithoutLeadingOrTrailingEmptyLines;
+}
 
 export class CodeBlock extends PureComponent<CodeBlockProps, any> {
   public element: HTMLPreElement;
