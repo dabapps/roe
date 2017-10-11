ff855b86a592a8ef84f78899453cba601d8ffa52
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-11 21:35:26 +0100

    Ignore index file

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   components: 'src/ts/**/*.{ts,tsx}',
+  ignore: ['**/ts/index.ts'],
   propsParser: require('react-docgen-typescript').parse,
   webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js')
 };
