4915c6b5639679bcf284b0c6d77c96e85f2c4487
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-11 16:40:05 +0000

    Update tslint and fix errors

--- src/ts/components/prototyping/dab-ipsum.tsx
+++ src/ts/components/prototyping/dab-ipsum.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HTMLProps, StatelessComponent } from 'react';
+import { StatelessComponent } from 'react';
 import { generateIpsum } from '../../utils';
 import { WORDS } from '../../words';
 
