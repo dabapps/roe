b2fbc739d50d0b9b2cc08efed44c8ddd07030910
Jake 'Sid' Smith <jake@dabapps.com>
2017-12-07 15:17:05 +0000

    Begin testing collapse component

---
+++ tests/collapse.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import { Collapse } from '../src/ts/';
+
+describe('Collapse', () => {
+
+  const createNodeMock = () => ({
+    scrollHeight: 500
+  });
+
+  it('should match snapshot when collapsed', () => {
+    const tree = renderer.create(
+      <Collapse open={false} />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should match snapshot when expanded', () => {
+    const tree = renderer.create(
+      <Collapse open />,
+      {createNodeMock}
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should take regular element attributes', () => {
+    const tree = renderer.create(
+      <Collapse open={false} className="my-class" />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+});
