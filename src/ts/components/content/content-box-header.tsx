33cf26c704768e8d186d4d8f03616dbaf9e77511
Jake 'Sid' Smith <jake@dabapps.com>
2017-12-15 13:25:56 +0000

    Update content components

--- src/ts/components/content/content-box-header.tsx
+++ src/ts/components/content/content-box-header.tsx
@@ -1,7 +1,6 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
-import { HTMLProps } from 'react';
-import { StatelessComponent } from 'react';
+import { HTMLProps, PureComponent } from 'react';
 import { ComponentProps } from '../../types';
 
 export type ContentBoxHeaderProps = ComponentProps & HTMLProps<HTMLElement>;
@@ -10,19 +9,19 @@ export type ContentBoxHeaderProps = ComponentProps & HTMLProps<HTMLElement>;
  * Footer for `ContentBox`s, used to display a content's title.
  * See the [ContentBox](#contentbox) section for a full example.
  */
-export const ContentBoxHeader: StatelessComponent<ContentBoxHeaderProps> = (props) => {
-  const {
-    className,
-    children,
-    component: Component = 'div',
-    ...remainingProps
-  } = props;
+export default class ContentBoxHeader extends PureComponent<ContentBoxHeaderProps, {}> {
+  public render () {
+    const {
+      className,
+      children,
+      component: Component = 'div',
+      ...remainingProps
+    } = this.props;
 
-  return (
-    <Component {...remainingProps} className={classNames('content-box-header', className)}>
-      {children}
-    </Component>
-  );
+    return (
+      <Component {...remainingProps} className={classNames('content-box-header', className)}>
+        {children}
+      </Component>
+    );
+  }
 }
-
-export default ContentBoxHeader;
--- src/ts/components/content/content-box.tsx
+++ src/ts/components/content/content-box.tsx
@@ -1,7 +1,6 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
-import { HTMLProps } from 'react';
-import { StatelessComponent } from 'react';
+import { HTMLProps, PureComponent } from 'react';
 import { ComponentProps } from '../../types';
 
 export type ContentBoxProps = ComponentProps & HTMLProps<HTMLElement>;
@@ -9,19 +8,19 @@ export type ContentBoxProps = ComponentProps & HTMLProps<HTMLElement>;
 /**
  * Box for displaying content within.
  */
-export const ContentBox: StatelessComponent<ContentBoxProps> = (props) => {
-  const {
-    className,
-    children,
-    component: Component = 'div',
-    ...remainingProps
-  } = props;
+export default class ContentBox extends PureComponent<ContentBoxProps, {}> {
+  public render () {
+    const {
+      className,
+      children,
+      component: Component = 'div',
+      ...remainingProps
+    } = this.props;
 
-  return (
-    <Component {...remainingProps} className={classNames('content-box', className)}>
-      {children}
-    </Component>
-  );
+    return (
+      <Component {...remainingProps} className={classNames('content-box', className)}>
+        {children}
+      </Component>
+    );
+  }
 }
-
-export default ContentBox;
