f07eb262b79c35d8672d17454acba0a949c53e81
Nikolaev Tomov <nikolaevtomov@gmail.com>
2018-03-06 15:47:06 +0000

    Translating banner

--- src/ts/components/banner/index.tsx
+++ src/ts/components/banner/index.tsx
@@ -4,28 +4,27 @@ import { HTMLProps, PureComponent } from 'react';
 import * as CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import { ComponentProps } from '../../types';
 
-export type BannerProps = ComponentProps & HTMLProps<HTMLElement>;
+export interface BannerProps extends ComponentProps, HTMLProps<HTMLElement> {
+  open?: boolean;
+}
 
 export class Banner extends PureComponent<BannerProps, {}> {
   public render() {
     const {
       className,
       children,
-      height = 50,
+      open = true,
       component: Component = 'div',
       ...remainingProps
     } = this.props;
 
     return (
-      <CSSTransitionGroup
-        transitionName="banner-transition"
-        transitionEnterTimeout={300}
-        transitionLeaveTimeout={200}
+      <Component
+        {...remainingProps}
+        className={classNames('banner', open && 'open', className)}
       >
-        <Component {...remainingProps} style={{height}} className={classNames('banner', className)}>
-          {children}
-        </Component>
-      </CSSTransitionGroup>
+        {children}
+      </Component>
     );
   }
 }
