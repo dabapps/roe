b07bb23a199510b485b2a18a1b6b7bb7ec378f0d
Jake 'Sid' Smith <jake@dabapps.com>
2018-03-08 17:35:23 +0000

    onClickoutside Sidebar

--- src/ts/components/navigation/sidebar.tsx
+++ src/ts/components/navigation/sidebar.tsx
@@ -4,10 +4,23 @@ import { HTMLProps, PureComponent } from 'react';
 import { ComponentProps } from '../../types';
 
 export interface SidebarProps extends HTMLProps<HTMLElement>, ComponentProps {
+  /**
+   * Sidebar is hidden off screen if this is falsy.
+   */
   open?: boolean;
+  /**
+   * Position the Sidebar to the left or right of the screen.
+   */
   position: 'left' | 'right';
+  /**
+   * Callback to trigger when the user clicks outside of the `Sidebar`.
+   */
+  onClickOutside(event: React.MouseEvent<HTMLDivElement>): void;
 }
 
+/**
+ * Sidebar navigation that opens over the content.
+ */
 export class Sidebar extends PureComponent<SidebarProps, {}> {
   public render () {
     const {
@@ -15,17 +28,21 @@ export class Sidebar extends PureComponent<SidebarProps, {}> {
       children,
       open,
       position,
+      onClickOutside,
       component: Component = 'div',
       ...remainingProps
     } = this.props;
 
     return (
-      <Component
-        {...remainingProps}
-        className={classNames('sidebar', position, open && 'open', className)}
-      >
-        {children}
-      </Component>
+      <div>
+        <div className="sidebar-overlay" onClick={onClickOutside} />
+        <Component
+          {...remainingProps}
+          className={classNames('sidebar', position, open && 'open', className)}
+        >
+          {children}
+        </Component>
+      </div>
     );
   }
 }
