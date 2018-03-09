504134cfb26c4fb73cd1a8c87d77a87f61b110a4
Jake 'Sid' Smith <jake@dabapps.com>
2018-03-09 18:26:34 +0000

    Note about why <a> is not included with NavItems

--- src/ts/components/navigation/nav-item.tsx
+++ src/ts/components/navigation/nav-item.tsx
@@ -12,7 +12,8 @@ export interface NavItemProps extends ComponentProps, HTMLProps<HTMLElement> {
 
 /**
  * NavItems are used inside of a Nav. These already have basic hover styles applied.
- * You should always nest an `<a>` inside a NavItem.
+ * You should **always** nest an `<a>` inside a NavItem. This is not part of the component so that you can use other
+ * components in place of an `<a>`, such as a React Router link (which renders an `<a>` also).
  * These can have an active class applied to them to highlight the currently active page.
  * You may apply `button` and related classes to a NavItem e.g. for a logout button.
  * See the [Nav](#nav) section for a full example.
