a3239aa37d3616e5dc4ce194c1cad0153553d2b7
Jake 'Sid' Smith <jake@dabapps.com>
2018-03-10 15:37:15 +0000

    Test NavItem

---
+++ tests/nav-item.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import NavItem from '../src/ts/components/navigation/nav-item';
+
+describe('NavItem', () => {
+
+  it('should match snapshot', () => {
+    const tree = renderer.create(<NavItem />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should accept regular element attributes', () => {
+    const tree = renderer.create(<NavItem className="my-class" />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should apply an active class', () => {
+    const tree = renderer.create(<NavItem active />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+});
