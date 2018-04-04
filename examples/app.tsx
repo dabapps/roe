2ba97ded2b86bedc675af2b9ce766d33ff57a7e1
Nikolaev Tomov <nikolaevtomov@gmail.com>
2018-04-04 11:38:30 +0100

    Badge component

--- examples/app.tsx
+++ examples/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { PureComponent } from 'react';
 import {
   AppRoot,
+  Badge,
   Button,
   Column,
   Container,
@@ -71,6 +72,14 @@ class App extends PureComponent<{}, AppState> {
 
         <Container>
           <h1>Example</h1>
+
+          <Badge small className={'info'}>
+            Niko is Awesome!!
+          </Badge>
+
+          <Badge small className={'primary'}>
+            5
+          </Badge>
 
           <DabIpsum />
 
---
+++ src/ts/components/badges/badge.tsx
@@ -0,0 +1,47 @@
+import * as classNames from 'classnames';
+import * as React from 'react';
+import { HTMLProps, PureComponent } from 'react';
+import { ComponentProps } from '../../types';
+
+export interface BadgeProps extends ComponentProps, HTMLProps<HTMLElement> {
+  /**
+   * Make the Badge small
+   */
+  small?: boolean;
+  /**
+   * Make the Badge large
+   */
+  large?: boolean;
+}
+
+/**
+ * Badge Component
+ */
+export class Badge extends PureComponent<BadgeProps, {}> {
+	public render() {
+		const {
+      className,
+      small,
+      large,
+      children,
+			component: Component = 'div',
+			...remainingProps
+    } = this.props;
+
+		return (
+			<Component
+        {...remainingProps}
+        className={classNames(
+          'badge',
+          small ? 'small' : null,
+          large ? 'large' : null,
+          className
+        )}
+			>
+			  {children}
+			</Component>
+    );
+	}
+}
+
+export default Badge;
