253a3eefa1822cfed5e86ed2b8b1abc13d4b21c0
Jake 'Sid' Smith <jake@dabapps.com>
2018-07-23 15:29:31 +0100

    Update tests

--- tests/speech-bubble.tsx
+++ tests/speech-bubble.tsx
@@ -1,24 +1,34 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
-import { Speech } from '../src/ts/components/speech';
+import { SpeechBubble } from '../src/ts/components/speech-bubble';
 
-describe('Speech', () => {
-  it('should match snapshot sent', () => {
+describe('SpeechBubble', () => {
+  it('should match snapshot', () => {
     const tree = renderer.create(
-      <Speech sent>
+      <SpeechBubble>
         <p>I love kittens!</p>
-      </Speech>
+      </SpeechBubble>
     );
 
     expect(tree).toMatchSnapshot();
   });
 
-  it('should match snapshot received', () => {
+  it('should match snapshot tailPosition="right"', () => {
     const tree = renderer.create(
-      <Speech received>
+      <SpeechBubble tailPosition="right">
         <p>I love puggs!</p>
-      </Speech>
+      </SpeechBubble>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should match accept regular attributes', () => {
+    const tree = renderer.create(
+      <SpeechBubble className="primary">
+        <p>I love puggs!</p>
+      </SpeechBubble>
     );
 
     expect(tree).toMatchSnapshot();
