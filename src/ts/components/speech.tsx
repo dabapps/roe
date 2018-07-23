4479d835045a86694fa7b102bbb5b10192150fa7
Jake 'Sid' Smith <jake@dabapps.com>
2018-07-23 10:59:40 +0100

    Update description

--- src/ts/components/speech.tsx
+++ src/ts/components/speech.tsx
@@ -4,17 +4,18 @@ import { HTMLProps, PureComponent } from 'react';
 import { ComponentProps } from '../types';
 
 export interface SpeechProps extends ComponentProps, HTMLProps<HTMLElement> {
-/**
- *  Position the speech bubble tail on the right
- */
-  sent ?: boolean;
-/**
- * Position the speech bubble tail on the left
- */
-  received ?: boolean;
+  /**
+   *  Position the speech bubble tail on the right
+   */
+  sent?: boolean;
+  /**
+   * Position the speech bubble tail on the left
+   */
+  received?: boolean;
 }
+
 /**
- * Speech Bubble Component
+ * Speech bubble component for displaying conversations / messages.
  */
 export class Speech extends PureComponent<SpeechProps, {}> {
   public render() {
