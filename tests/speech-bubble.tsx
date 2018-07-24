7054dc11ae5cdf9d79c0f1e7e0ba6c9b986cd45e
Jake 'Sid' Smith <jake@dabapps.com>
2018-07-24 10:15:13 +0100

    Additional tests

--- tests/speech-bubble.tsx
+++ tests/speech-bubble.tsx
@@ -43,4 +43,30 @@ describe('SpeechBubble', () => {
 
     expect(tree).toMatchSnapshot();
   });
+
+  it('should display header content above the speech bubble', () => {
+    const tree = renderer.create(
+      <SpeechBubble
+        header={
+          <span>
+            <strong>Username</strong> <span className="info">1 minute ago</span>
+          </span>
+        }
+      >
+        <p>I love puggs!</p>
+      </SpeechBubble>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should display footer content below the speech bubble', () => {
+    const tree = renderer.create(
+      <SpeechBubble footer={<span className="info italic">Seen</span>}>
+        <p>I love puggs!</p>
+      </SpeechBubble>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
 });
