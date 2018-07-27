0ff5a671adddf89d4f710f0197e8fd28d53ebdd8
Jake 'Sid' Smith <jake@dabapps.com>
2018-07-27 17:59:41 +0100

    Simplify tests

--- tests/highlight.tsx
+++ tests/highlight.tsx
@@ -13,7 +13,7 @@ describe('Highlight', () => {
   it('should match snapshot', () => {
     const tree = renderer.create(
       <Highlight>
-        <input type="number" />
+        <p>Hello, World!</p>
       </Highlight>
     );
 
@@ -22,15 +22,9 @@ describe('Highlight', () => {
 
   it('should match snapshot with props (open)', () => {
     const tree = renderer.create(
-      <Row>
-        <Column>
-          <Highlight open>
-            <InputGroup>
-              <input type="number" />
-            </InputGroup>
-          </Highlight>
-        </Column>
-      </Row>
+      <Highlight open>
+        <p>Hello, World!</p>
+      </Highlight>
     );
 
     expect(tree).toMatchSnapshot();
@@ -38,16 +32,9 @@ describe('Highlight', () => {
 
   it('should match snapshot with props (open, disabled, backgroundColor)', () => {
     const tree = renderer.create(
-      <Row>
-        <Column>
-          <Highlight open disabled backgroundColor="white">
-            <InputGroup>
-              <InputGroupAddon>£</InputGroupAddon>
-              <input type="number" />
-            </InputGroup>
-          </Highlight>
-        </Column>
-      </Row>
+      <Highlight open disabled backgroundColor="white">
+        <p>Hello, World!</p>
+      </Highlight>
     );
 
     expect(tree).toMatchSnapshot();
