91acf40539bcb31f307f78a58d780d426696e362
Al Power <al@dabapps.com>
2018-09-26 15:02:11 +0100

    linting

--- examples/app.tsx
+++ examples/app.tsx
@@ -138,7 +138,9 @@ class App extends PureComponent<{}, AppState> {
               </InputGroup>
 
               <InputGroup block>
-                <InputGroupAddon><span>$</span></InputGroupAddon>
+                <InputGroupAddon>
+                  <span>$</span>
+                </InputGroupAddon>
                 <input type="number" />
                 <Button component="div" tabIndex={0}>
                   Done
