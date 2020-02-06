7e8070226b633b88049c85946034b4cacffdd6c2
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-02-06 17:25:32 +0000

    Update test description

--- tests/pagination-display.tsx
+++ tests/pagination-display.tsx
@@ -3,8 +3,8 @@ import * as renderer from 'react-test-renderer';
 
 import { PaginationDisplay } from '../src/ts';
 
-describe('Banner', () => {
-  it('should match snapshot', () => {
+describe('PaginationDisplay', () => {
+  it('should match snapshot with a text (Showing), item count and indication of diplaying 3 items', () => {
     const tree = renderer.create(
       <PaginationDisplay
         className="margin-top-base"
