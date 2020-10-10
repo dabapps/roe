6a4769fbb7c03d803c4d4a41dbdd7aa4f814957e
Jake 'Sid' Smith <jake@dabapps.com>
2020-10-10 13:04:58 +0100

    Update side bar transitions

--- src/ts/components/navigation/side-bar.tsx
+++ src/ts/components/navigation/side-bar.tsx
@@ -1,7 +1,7 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
 import { HTMLProps, PureComponent } from 'react';
-import * as CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { ComponentProps } from '../../types';
 
 export interface SideBarProps extends HTMLProps<HTMLElement>, ComponentProps {
@@ -23,6 +23,12 @@ export interface SideBarProps extends HTMLProps<HTMLElement>, ComponentProps {
   onClickOutside(event: React.MouseEvent<HTMLDivElement>): void;
 }
 
+const TIMEOUT = {
+  appear: 300,
+  enter: 300,
+  exit: 200,
+};
+
 /**
  * SideBar navigation that opens over the content. Often used as the primary navigation on small devices.
  * See the [Nav](#nav) section for more details.
@@ -42,15 +48,13 @@ export class SideBar extends PureComponent<SideBarProps, {}> {
 
     return (
       <div className={classNames('side-bar-container', className)}>
-        <CSSTransitionGroup
-          transitionName="side-bar-transition"
-          transitionEnterTimeout={300}
-          transitionLeaveTimeout={200}
-        >
+        <TransitionGroup>
           {open && (
-            <div className="side-bar-overlay" onClick={onClickOutside} />
+            <CSSTransition classNames="side-bar-transition" timeout={TIMEOUT}>
+              <div className="side-bar-overlay" onClick={onClickOutside} />
+            </CSSTransition>
           )}
-        </CSSTransitionGroup>
+        </TransitionGroup>
         <Component
           {...remainingProps}
           className={classNames(
