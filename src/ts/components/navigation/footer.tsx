fcd153bf1ebd23e87d20f75b3b65ed6ad9ee82c5
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-11-10 15:09:25 +0000

    Update

--- src/ts/components/navigation/footer.tsx
+++ src/ts/components/navigation/footer.tsx
@@ -20,7 +20,6 @@ export interface FooterProps extends ComponentProps, HTMLProps<HTMLElement> {
 
 export class Footer extends PureComponent<FooterProps, {}> {
   public componentDidMount() {
-    this.notifyAppRoot(this.props);
     this.toggleResizeListeners(this.props);
   }
 
@@ -31,13 +30,10 @@ export class Footer extends PureComponent<FooterProps, {}> {
     ) {
       this.toggleResizeListeners(this.props);
     }
-
-    this.notifyAppRoot(this.props);
   }
 
   public componentWillUnmount() {
-    window.removeEventListener('resize', this.updateAppRoot);
-    this.notifyAppRoot({ sticky: false });
+    this.observer(this.props).disconnect();
   }
 
   public render() {
@@ -67,9 +63,6 @@ export class Footer extends PureComponent<FooterProps, {}> {
       for (const entry of entries) {
         const {height} = entry.contentRect;
 
-        // console.log('Element:', entry.target);
-        // console.log(`Element's size: ${ width }px x ${ height }px`);
-
         store.setState({
           hasStickyFooter: Boolean(sticky || fixed),
           footerHeight:
@@ -81,22 +74,13 @@ export class Footer extends PureComponent<FooterProps, {}> {
     }
   );
 
-  private notifyAppRoot(props: FooterProps) {
-    const element = ReactDOM.findDOMNode(this);
-    this.observer(this.props).observe(element);
-  }
-
-  private updateAppRoot = () => {
-    this.notifyAppRoot(this.props);
-  };
-
   private toggleResizeListeners(props: FooterProps) {
     const { sticky, fixed } = props;
 
     if (sticky || fixed) {
-      window.addEventListener('resize', this.updateAppRoot);
+      this.observer(this.props).observe(ReactDOM.findDOMNode(this));
     } else {
-      window.removeEventListener('resize', this.updateAppRoot);
+      this.observer(this.props).disconnect();
     }
   }
 }
