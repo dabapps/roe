0d6fa98879fbfd41a34fc80b8dc36e11328728ae
Nikolaev Tomov <nikolaev.github@yahoo.com>
2021-03-08 11:41:57 +0000

    Banner to func comp

--- src/ts/components/banners/banner.tsx
+++ src/ts/components/banners/banner.tsx
@@ -1,9 +1,11 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
-import { HTMLProps, PureComponent } from 'react';
+
 import { ComponentProps } from '../../types';
 
-export interface BannerProps extends ComponentProps, HTMLProps<HTMLElement> {
+export interface BannerProps
+  extends ComponentProps,
+    React.HTMLProps<HTMLElement> {
   /**
    * If set, displays the component, otherwise it is hidden
    * @default true
@@ -19,26 +21,24 @@ export interface BannerProps extends ComponentProps, HTMLProps<HTMLElement> {
 /**
  * A Banner component that displays fixed to the top or bottom of the screen.
  */
-export class Banner extends PureComponent<BannerProps, {}> {
-  public render() {
-    const {
-      className,
-      children,
-      open = true,
-      position = 'bottom',
-      component: Component = 'div',
-      ...remainingProps
-    } = this.props;
+const Banner = (props: BannerProps): React.ReactElement => {
+  const {
+    className,
+    children,
+    open = true,
+    position = 'bottom',
+    component: Component = 'div',
+    ...remainingProps
+  } = props;
 
-    return (
-      <Component
-        {...remainingProps}
-        className={classNames('banner', open && 'open', position, className)}
-      >
-        {children}
-      </Component>
-    );
-  }
-}
+  return (
+    <Component
+      {...remainingProps}
+      className={classNames('banner', open && 'open', position, className)}
+    >
+      {children}
+    </Component>
+  );
+};
 
 export default Banner;
