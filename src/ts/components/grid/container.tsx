2039a3a8839d7a6c49f1dcb71f78e05bfb3c4bd5
Nikolaev Tomov <nikolaev.github@yahoo.com>
2021-03-08 12:52:55 +0000

    Grid to funct comp

--- src/ts/components/grid/container.tsx
+++ src/ts/components/grid/container.tsx
@@ -1,9 +1,11 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
-import { HTMLProps, PureComponent } from 'react';
+
 import { ComponentProps } from '../../types';
 
-export interface ContainerProps extends ComponentProps, HTMLProps<HTMLElement> {
+export interface ContainerProps
+  extends ComponentProps,
+    React.HTMLProps<HTMLElement> {
   /**
    * Fill parent with no media queries to affect width.
    */
@@ -17,28 +19,27 @@ export interface ContainerProps extends ComponentProps, HTMLProps<HTMLElement> {
 /**
  * Used inside `NavBar`s or as the main wrapper for an application.
  */
-export class Container extends PureComponent<ContainerProps, {}> {
-  public render() {
-    const {
-      children,
-      className,
-      fluid,
-      solid,
-      component: Component = 'div',
-      ...remainingProps
-    } = this.props;
-    const fluidClassName = fluid ? 'container-fluid' : 'container';
-    const solidClassName = solid && 'solid';
+const Container = (props: ContainerProps) => {
+  const {
+    children,
+    className,
+    fluid,
+    solid,
+    component: Component = 'div',
+    ...remainingProps
+  } = props;
 
-    return (
-      <Component
-        {...remainingProps}
-        className={classNames(fluidClassName, solidClassName, className)}
-      >
-        {children}
-      </Component>
-    );
-  }
-}
+  const fluidClassName = fluid ? 'container-fluid' : 'container';
+  const solidClassName = solid && 'solid';
 
-export default Container;
+  return (
+    <Component
+      {...remainingProps}
+      className={classNames(fluidClassName, solidClassName, className)}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export default React.memo(Container);
