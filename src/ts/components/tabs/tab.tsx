e8fafe3c16b34e083800d30a43ff97c2d66dcd4e
Nikolaev Tomov <nikolaev.github@yahoo.com>
2021-03-08 14:40:53 +0000

    tabs to funct comp

--- src/ts/components/tabs/tab.tsx
+++ src/ts/components/tabs/tab.tsx
@@ -1,9 +1,9 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
-import { HTMLProps, PureComponent } from 'react';
+
 import { ComponentProps } from '../../types';
 
-export interface TabProps extends ComponentProps, HTMLProps<HTMLElement> {
+export interface TabProps extends ComponentProps, React.HTMLProps<HTMLElement> {
   /**
    * Apply active `Tab` styles.
    */
@@ -15,25 +15,23 @@ export interface TabProps extends ComponentProps, HTMLProps<HTMLElement> {
  * Easily style active tabs with the `active` prop.
  * See the [Tabs](#tabs) section for a full example.
  */
-export class Tab extends PureComponent<TabProps, {}> {
-  public render() {
-    const {
-      className,
-      children,
-      active,
-      component: Component = 'li',
-      ...remainingProps
-    } = this.props;
+const Tab = (props: TabProps) => {
+  const {
+    className,
+    children,
+    active,
+    component: Component = 'li',
+    ...remainingProps
+  } = props;
 
-    return (
-      <Component
-        {...remainingProps}
-        className={classNames('tab', active && 'active', className)}
-      >
-        {children}
-      </Component>
-    );
-  }
-}
+  return (
+    <Component
+      {...remainingProps}
+      className={classNames('tab', active && 'active', className)}
+    >
+      {children}
+    </Component>
+  );
+};
 
-export default Tab;
+export default React.memo(Tab);
