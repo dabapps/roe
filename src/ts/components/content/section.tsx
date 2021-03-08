3f3af7540a944a4388bd069f7170df183369835e
Nikolaev Tomov <nikolaev.github@yahoo.com>
2021-03-08 12:43:01 +0000

    Section to funct comp

--- src/ts/components/content/section.tsx
+++ src/ts/components/content/section.tsx
@@ -1,32 +1,30 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
-import { HTMLProps, PureComponent } from 'react';
+
 import { ComponentProps } from '../../types';
 
-export type SectionProps = ComponentProps & HTMLProps<HTMLElement>;
+export type SectionProps = ComponentProps & React.HTMLProps<HTMLElement>;
 
 /**
  * Used to separate sections of content with a horizontal-rule-like style.
  * Should only be used inside `ContentBox`s or `Column`s.
  */
-export class Section extends PureComponent<SectionProps, {}> {
-  public render() {
-    const {
-      children,
-      className,
-      component: Component = 'section',
-      ...remainingProps
-    } = this.props;
+const Section = (props: SectionProps) => {
+  const {
+    children,
+    className,
+    component: Component = 'section',
+    ...remainingProps
+  } = props;
 
-    return (
-      <Component
-        {...remainingProps}
-        className={classNames(['section', className])}
-      >
-        {children}
-      </Component>
-    );
-  }
-}
+  return (
+    <Component
+      {...remainingProps}
+      className={classNames(['section', className])}
+    >
+      {children}
+    </Component>
+  );
+};
 
-export default Section;
+export default React.memo(Section);
