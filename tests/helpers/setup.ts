c315ee9d4b048245947d4836d80fb8b9cb25abe2
Nikolaev Tomov <nikolaev.github@yahoo.com>
2021-03-08 10:18:20 +0000

    Install react v16.8 and dependencies

--- tests/helpers/setup.ts
+++ tests/helpers/setup.ts
@@ -1,18 +1,18 @@
 import * as enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-15';
+import * as Adapter from 'enzyme-adapter-react-16';
 
 enzyme.configure({ adapter: new Adapter() });
 
 // This monkey patches document.createEvent so that MouseEvents have a `pageX` value.
 // This is intended to prevent React from adding window listeners when a component is mounted using enzyme.
-const originalCreateEvent = document.createEvent;
+// const originalCreateEvent = document.createEvent;
 
-document.createEvent = function(type: string): any {
-  const event = originalCreateEvent.call(this, type);
+// document.createEvent = function(type: string): any {
+//   const event = originalCreateEvent.call(this, type);
 
-  if (type === 'MouseEvent') {
-    (event as any).pageX = 0;
-  }
+//   if (type === 'MouseEvent') {
+//     (event as any).pageX = 0;
+//   }
 
-  return event;
-};
+//   return event;
+// };
