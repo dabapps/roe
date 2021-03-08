0e403fd9017d61543dec19f89d7a9ea2aee00b13
Nikolaev Tomov <nikolaev.github@yahoo.com>
2021-03-08 12:12:42 +0000

    ContentBoxFooter to func comp

--- src/ts/components/content/content-box-footer.tsx
+++ src/ts/components/content/content-box-footer.tsx
@@ -1,32 +1,31 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
-import { HTMLProps, PureComponent } from 'react';
+
 import { ComponentProps } from '../../types';
 
-export type ContentBoxFooterProps = ComponentProps & HTMLProps<HTMLElement>;
+export type ContentBoxFooterProps = ComponentProps &
+  React.HTMLProps<HTMLElement>;
 
 /**
  * Footer for `ContentBox`s, ideal for submit buttons, links & sub-text.
  * See the [ContentBox](#contentbox) section for a full example.
  */
-export class ContentBoxFooter extends PureComponent<ContentBoxFooterProps, {}> {
-  public render() {
-    const {
-      className,
-      children,
-      component: Component = 'div',
-      ...remainingProps
-    } = this.props;
+const ContentBoxFooter = (props: ContentBoxFooterProps) => {
+  const {
+    className,
+    children,
+    component: Component = 'div',
+    ...remainingProps
+  } = props;
 
-    return (
-      <Component
-        {...remainingProps}
-        className={classNames('content-box-footer', className)}
-      >
-        {children}
-      </Component>
-    );
-  }
-}
+  return (
+    <Component
+      {...remainingProps}
+      className={classNames('content-box-footer', className)}
+    >
+      {children}
+    </Component>
+  );
+};
 
-export default ContentBoxFooter;
+export default React.memo(ContentBoxFooter);
