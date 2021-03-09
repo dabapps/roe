4e61fb548f89b8d40ebcc63e5b7ec6c395c2b2d1
Jake 'Sid' Smith <jake@dabapps.com>
2021-03-09 10:36:07 +0000

    Fix syntax error

--- src/ts/components/content/content-box-header.tsx
+++ src/ts/components/content/content-box-header.tsx
@@ -3,29 +3,29 @@ import * as React from 'react';
 
 import { ComponentProps } from '../../types';
 
-export type ContentBoxHeaderProps = ComponentProps & React.HTMLProps<HTMLElement>;
+export type ContentBoxHeaderProps = ComponentProps &
+  React.HTMLProps<HTMLElement>;
 
 /**
  * Header for `ContentBox`s, used to display a content's title.
  * See the [ContentBox](#contentbox) section for a full example.
  */
 const ContentBoxHeader = (props: ContentBoxHeaderProps) => {
-    const {
-      className,
-      children,
-      component: Component = 'div',
-      ...remainingProps
-    } = props;
+  const {
+    className,
+    children,
+    component: Component = 'div',
+    ...remainingProps
+  } = props;
 
-    return (
-      <Component
-        {...remainingProps}
-        className={classNames('content-box-header', className)}
-      >
-        {children}
-      </Component>
-    );
-  }
-}
+  return (
+    <Component
+      {...remainingProps}
+      className={classNames('content-box-header', className)}
+    >
+      {children}
+    </Component>
+  );
+};
 
 export default React.memo(ContentBoxHeader);
