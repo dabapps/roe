4e8ce5f11de0e0daf6aa754fac05f20c86308393
Jake 'Sid' Smith <jake@dabapps.com>
2021-03-09 10:29:00 +0000

    Add new types for optional component prop

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,8 +1,35 @@
-export interface ComponentProps {
+export type IntrinsicElementType = keyof JSX.IntrinsicElements;
+
+export type NoComponentProp<D extends IntrinsicElementType> = {
   /**
    * Set the component to render a different element type.
    */
-  component?: string;
+  component?: undefined;
+} & JSX.IntrinsicElements[D];
+
+export type WithComponentProp<C extends IntrinsicElementType> = {
+  /**
+   * Set the component to render a different element type.
+   */
+  component: C;
+} & JSX.IntrinsicElements[C];
+
+export type OptionalComponentProp<C extends IntrinsicElementType> = {
+  /**
+   * Set the component to render a different element type.
+   */
+  component?: C;
+} & JSX.IntrinsicElements[C];
+
+export interface FunctionComponentOptionalComponentProp<
+  D extends IntrinsicElementType,
+  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+  E extends Record<string, any> = {}
+> {
+  (props: NoComponentProp<D> & E): React.ReactElement;
+  <C extends IntrinsicElementType>(
+    props: WithComponentProp<C> & E
+  ): React.ReactElement;
 }
 
 export interface BaseTableCellProps extends ComponentProps {
