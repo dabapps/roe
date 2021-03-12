1c531579aa9c643ecbce35a77f9854307bcfd947
Jake 'Sid' Smith <jake@dabapps.com>
2021-03-12 13:17:10 +0000

    Update AppRoot tests

--- tests/root.tsx
+++ tests/root.tsx
@@ -1,21 +1,21 @@
-import * as enzyme from 'enzyme';
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-
-import { AppRoot } from '../src/ts';
-import store from '../src/ts/store';
 
 const mockUnsubscribe = jest.fn();
 const mockSubscribe = jest.fn().mockReturnValue(mockUnsubscribe);
 const mockGetState = jest.fn().mockReturnValue({});
+const mockUseState = jest.fn().mockReturnValue({});
 
 jest.mock('../src/ts/store', () => ({
   default: {
     getState: mockGetState,
     subscribe: mockSubscribe,
     unsubscribe: mockUnsubscribe,
+    useState: mockUseState,
   },
 }));
+
+import { AppRoot } from '../src/ts';
 
 describe('AppRoot', () => {
   beforeEach(() => {
@@ -36,7 +36,7 @@ describe('AppRoot', () => {
   });
 
   it('should apply classes for fixed nav bar and sticky footer', () => {
-    mockGetState.mockReturnValue({
+    mockUseState.mockReturnValue({
       hasFixedNavBar: true,
       hasStickyFooter: true,
     });
@@ -47,7 +47,7 @@ describe('AppRoot', () => {
   });
 
   it('should apply padding for fixed nav bar and sticky footer', () => {
-    mockGetState.mockReturnValue({
+    mockUseState.mockReturnValue({
       hasFixedNavBar: true,
       hasStickyFooter: true,
       navBarHeight: 50,
@@ -57,20 +57,5 @@ describe('AppRoot', () => {
     const tree = renderer.create(<AppRoot />);
 
     expect(tree).toMatchSnapshot();
-  });
-
-  it('should subscribe and unsubscribe from the store on mount and unmount', () => {
-    expect(store.subscribe).not.toHaveBeenCalled();
-    expect(mockUnsubscribe).not.toHaveBeenCalled();
-
-    const instance = enzyme.mount(<AppRoot />);
-
-    expect(store.subscribe).toHaveBeenCalled();
-    expect(mockUnsubscribe).not.toHaveBeenCalled();
-
-    instance.unmount();
-
-    expect(store.subscribe).toHaveBeenCalledTimes(1);
-    expect(mockUnsubscribe).toHaveBeenCalledTimes(1);
   });
 });
