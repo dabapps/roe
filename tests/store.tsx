803482fd75d08c14b3fdd486346368052b2a5b99
Jake 'Sid' Smith <jake@dabapps.com>
2021-03-12 13:50:43 +0000

    Test store's useState hook

--- tests/store.tsx
+++ tests/store.tsx
@@ -1,6 +1,19 @@
+const mockUseState = jest.fn().mockImplementation(input => [input, jest.fn()]);
+const mockUseEffect = jest.fn();
+
+jest.mock('react', () => ({
+  useState: mockUseState,
+  useEffect: mockUseEffect,
+}));
+
 import { Store } from '../src/ts/store';
 
 describe('store', () => {
+  beforeEach(() => {
+    mockUseState.mockClear();
+    mockUseEffect.mockClear();
+  });
+
   describe('Store', () => {
     it('should accept an initial state', () => {
       const testStore = new Store({
@@ -38,6 +51,33 @@ describe('store', () => {
       expect(listener).toHaveBeenCalledTimes(1);
 
       expect(unsubscribe).not.toThrow();
+    });
+
+    describe('useState', () => {
+      const testStore = new Store({ hasFixedNavBar: true });
+
+      it("should return the store's state", () => {
+        expect(testStore.useState()).toEqual({ hasFixedNavBar: true });
+      });
+
+      it('should subscribe on mount and unsubscribe on unmount', () => {
+        testStore.useState();
+
+        expect(testStore['listeners'].length).toBe(0);
+
+        expect(mockUseEffect).toHaveBeenCalledTimes(1);
+        expect(mockUseEffect).toHaveBeenCalledWith(expect.any(Function), []);
+
+        const [effectAdd] = mockUseEffect.mock.calls[0];
+
+        const effectRemove = effectAdd();
+
+        expect(testStore['listeners'].length).toBe(1);
+
+        effectRemove();
+
+        expect(testStore['listeners'].length).toBe(0);
+      });
     });
   });
 });
